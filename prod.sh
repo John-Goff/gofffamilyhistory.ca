77a2ac2e77959b6ae929fe3bf4384e52c914b1cb
John Goff <john.g@hiredhippo.ai>
2018-04-18 18:18:18 -0400

    fix dirname from $0 var

--- prod.sh
+++ prod.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 export PORT=80
+DIR=dirname "$0"
 
-until $0/gofffamilyhistory.ca; do
+until $DIR/gofffamilyhistory.ca; do
   echo "Server 'gofffamilyhistory' crashed with exit code $?.  Respawning.." >&2
   sleep 1
 done
